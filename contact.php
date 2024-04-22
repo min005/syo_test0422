e8dc13452c2ba189024049d33b09c17fc6678e93
min005 <137742387+min005@users.noreply.github.com>
2024-04-22 15:28:46 +0900

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,30 @@
+<?php
+mb_language("Japanese");
+mb_internal_encoding("UTF-8");
+
+if ($_POST) {
+    $to = 'xxxxxx@gmail.com';
+    // $to = 'miyuki.f1986@gmail.com';
+
+    $subject = 'メッセージが届きました';
+
+    $message = "メッセージが届きました。\n";
+    $message .= "入力された内容は以下の通りです。\n";
+    $message .= "---\n";
+    $message .= "\n";
+    $message .= "お名前：\n";
+    $message .= $_POST["name"]; 
+    $message .= "\n";
+    $message .= "メッセージ本文:\n";
+    $message .= $_POST["message"];
+
+    if (mb_send_mail($to, $subject, $message)) {
+        header('Location: thank_you.html');
+    } else {
+        echo "メールの送信に失敗しました";
+        echo "Failed to send email";
+    }
+} else {
+    echo "エラーが発生しました。もう一度お試しください。";
+    echo "An error has occurred. please try again.";
+}
---
+++ js/main.js
@@ -0,0 +1,100 @@
+//横スクロール
+$(function () {
+	const scrollElement = document.querySelector(".slides-wrapper");
+
+	scrollElement.addEventListener("wheel", (e) => {
+		if (Math.abs(e.deltaY) < Math.abs(e.deltaX)) return;
+		e.preventDefault();
+		scrollElement.scrollLeft += e.deltaY;
+	});
+});
+
+
+// mydad　画像選択→拡大
+
+$(function () {
+
+	$(".mydad_sub_img").slick({
+		infinite: false,
+		speed: 300,
+		slidesToShow: 4,
+		slidesToScroll: 2,
+		
+		responsive: [{
+			breakpoint: 600, // ブレイクポイントを指定
+			settings: {
+				arrows: false,
+				autoplay: true,
+				autoplaySpeed: 3000,
+				slidesToScroll: 1,
+				infinite: true,
+				slidesToShow: 3,
+				speed: 300,
+			},
+		},
+		]
+	});
+
+	$(".mydad_sub_img li").eq(0).addClass("select");
+
+	$(".mydad_sub_img img").click(function () {
+		var img = $(this).attr("src");
+
+		$(".mydad_sub_img li").removeClass("select");
+		$(this).parent().addClass("select");
+
+		$(".mydad_main img").fadeOut(500, function () {
+			$(this).attr("src", img),
+				$(this).fadeIn(500)
+		});
+	});
+});
+
+
+//gallery　画像選択→拡大
+
+$(function () {
+	if (window.matchMedia("(max-width: 767px)").matches) {
+		// windowのサイズが767px以下
+		$(".gallery_zoom").css('display', 'none');
+
+
+		$(".item").click(function () {
+			var img = $(this).children("img").attr("src");
+			var text = $(this).children("p").text();
+
+			$(".gallery_zoom").fadeOut(500, function () {
+				$(this).children("img").attr("src", img),
+					$(this).children("p").text(text),
+					$(this).fadeIn(300)
+			});
+
+			$(".gallery_zoom").addClass("open");
+			$("#js-overlay").addClass("open");
+		});
+
+		$("#js-close").click(function () {
+			$(".gallery_zoom").hide().removeClass("open");
+			$(".gallery_zoom").css('display', 'none');
+			$("#js-overlay").removeClass("open");
+		});
+
+
+	} else {
+		// windowのサイズが768px以上
+		$(".item").click(function () {
+			var img = $(this).children("img").attr("src");
+			var text = $(this).children("p").text();
+
+			$(".gallery_zoom").fadeOut(500, function () {
+				$(this).children("img").attr("src", img),
+					$(this).children("p").text(text),
+					$(this).fadeIn(300)
+			});
+
+			$(".gallery_zoom").addClass("open");
+		});
+	}
+});
+
+
