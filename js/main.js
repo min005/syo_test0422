04a800b4f0cdb3d3d99c16fb6a5b32d0011dd966
min005 <137742387+min005@users.noreply.github.com>
2024-04-23 11:47:03 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
 //横スクロール
 $(function () {
 	const scrollElement = document.querySelector("#slides-wrapper");
+	let touchStartX = 0;
+	let touchMoveX = 0;
+
+	// タッチ開始時のX座標を記録
+	scrollElement.addEventListener("touchstart", (e) => {
+		touchStartX = e.touches[0].clientX;
+	});
+
+	// タッチ移動時のX座標を記録し、スクロールさせる
+	scrollElement.addEventListener("touchmove", (e) => {
+		touchMoveX = e.touches[0].clientX;
+		const deltaX = touchStartX - touchMoveX;
+		scrollElement.scrollLeft += deltaX;
+		touchStartX = touchMoveX; // 次の移動のためにタッチ開始位置を更新
+		e.preventDefault(); // デフォルトのスワイプ動作を無効化
+	});
 
 	scrollElement.addEventListener("wheel", (e) => {
-		if (Math.abs(e.deltaY) < Math.abs(e.deltaX)) return;
-		e.preventDefault();
-		scrollElement.scrollLeft += e.deltaY;
-	});
-	
-	scrollElement.addEventListener("touchmove", (e) => {
 		if (Math.abs(e.deltaY) < Math.abs(e.deltaX)) return;
 		e.preventDefault();
 		scrollElement.scrollLeft += e.deltaY;
@@ -22,10 +32,10 @@ $(function () {
 
 	$(".mydad_sub_img").slick({
 		infinite: false,
+		arrows: true,
 		speed: 300,
 		slidesToShow: 4,
 		slidesToScroll: 2,
-
 		responsive: [{
 			breakpoint: 960,
 			settings: {
@@ -33,15 +43,19 @@ $(function () {
 				autoplay: true,
 				autoplaySpeed: 3000,
 				slidesToScroll: 2,
-				infinite: true,
-			},
+				infinite: true
+			}
+		}, {
 			breakpoint: 600,
 			settings: {
+				arrows: false,
+				autoplay: true,
+				autoplaySpeed: 3000,
 				slidesToScroll: 1,
 				slidesToShow: 3,
-			},
-		},
-		]
+				infinite: true
+			}
+		}]
 	});
 
 	$(".mydad_sub_img li").eq(0).addClass("select");
